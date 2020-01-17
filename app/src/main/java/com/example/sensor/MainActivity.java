5763d811dc59edf637fa17214137751642d95d03
dell <Khadkasantosh487@gmail.com>
2020-01-17 08:09:25 +0545

    Accelerometer

--- app/src/main/java/com/example/sensor/MainActivity.java
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.sensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private SensorManager sensorManager;
+    private TextView tvSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSensor = findViewById(R.id.tvSensors);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        for (int i = 0; i < sensorList.size(); i++) {
+            String sensors = "";
+            sensors += sensorList.get(i).getName() + "\n";
+            tvSensor.append(sensors);
+        }
+
     }
 }
